12b0581d942996ec74a6528a940b9504574e9712
Цезарь А. Альказар Ромеро <cesar.alcazar606@comunidadunir.net>
2025-02-08 17:41:52 +0100

    Subiendo la clase División otra vez otra vez otra vez otra vez otra vez y el Main

--- src/test/java/TestDivision.java
+++ src/test/java/TestDivision.java
@@ -1,15 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.unir.javabeans.Division;
+import org.unir.javabeans.DivisionCesarAAlcazarRomero;
 // Esto es una mierda
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDivision {
 
-    private Division division;
+    private DivisionCesarAAlcazarRomero division;
 
     @BeforeEach
-    void setUp() {division = new Division();}
+    void setUp() {division = new DivisionCesarAAlcazarRomero();}
 
     @Test
     void testDivisionReales() {
@@ -20,7 +20,7 @@ public class TestDivision {
 
     @Test
     void testDivisionEnteros() {
-        Division division = new Division();
+        DivisionCesarAAlcazarRomero division = new DivisionCesarAAlcazarRomero();
         assertEquals(10, division.divisionEnteros(20, 2), 0.0001);
         System.out.println("Debe dar 10: " + division.divisionEnteros(20, 2));
         assertEquals(Double.NaN, division.divisionEnteros(5, 0), 0.0001);
@@ -28,7 +28,7 @@ public class TestDivision {
 
     @Test
     void testInversoReal() {
-        Division division = new Division();
+        DivisionCesarAAlcazarRomero division = new DivisionCesarAAlcazarRomero();
         assertEquals(0.25, division.inversoReal(4), 0.0001);
         System.out.println("Debe dar 0.25: " + division.inversoReal(4));
         assertEquals(Double.NaN, division.inversoReal(0), 0.0001);
@@ -36,7 +36,7 @@ public class TestDivision {
 
     @Test
     void testRaiz(){
-        Division division = new Division();
+        DivisionCesarAAlcazarRomero division = new DivisionCesarAAlcazarRomero();
         assertEquals(15.0, division.raiz(225.0), 0.0001);
         System.out.println("Debe dar 15: " + division.raiz(225.0));
         assertEquals(Double.NaN, division.raiz(-144), 0.0001);
